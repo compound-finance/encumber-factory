cd86b39817c8c55a09a3a44c95792943aab95fd3
Scott Silver <scott@compound.finance>
2023-06-14 15:40:16 -0700

    simpify createWrapper

--- src/EncumbranceWrapperFactory.sol
+++ src/EncumbranceWrapperFactory.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.15;
 import "./EncumbranceWrapper.sol";
 
 contract EncumbranceWrapperFactory {
-    uint constant SALT = 0;
+    bytes32 constant SALT = "";
 
     function deploy(address underlyingToken) public returns (address) {
         EncumbranceWrapper wrapper = createWrapper(underlyingToken);
@@ -11,21 +11,7 @@ contract EncumbranceWrapperFactory {
     }
 
     function createWrapper(address underlyingToken) internal returns (EncumbranceWrapper) {
-        address payable addr;
-        bytes memory code = abi.encodePacked(
-            type(EncumbranceWrapper).creationCode,
-            abi.encode(underlyingToken)
-        );
-        uint salt = SALT;
-
-        assembly {
-            addr := create2(0, add(code, 0x20), mload(code), salt)
-            if iszero(extcodesize(addr)) {
-                revert(0, 0)
-            }
-        }
-
-        return EncumbranceWrapper(addr);
+        return new EncumbranceWrapper{salt: ""}(underlyingToken);
     }
 
     function getDeploymentAddress(address underlyingToken) public view returns (address) {
