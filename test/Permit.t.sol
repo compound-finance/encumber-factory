53f84dc0d829d507c5e82f74c8c6e1a9103b6b04
Scott Silver <scott@compound.finance>
2023-06-22 14:34:52 -0700

    update Permit comments

--- test/Permit.t.sol
+++ test/Permit.t.sol
@@ -151,7 +151,7 @@ contract PermitTest is Test {
 
         (uint8 v, bytes32 r, bytes32 s) = aliceAuthorization(allowance, badNonce, expiry);
 
-        // bob calls permit with the signature, but he manipulates the expiry
+        // bob calls permit with the signature with an invalid nonce
         vm.prank(bob);
         vm.expectRevert("Bad signatory");
         wrappedToken.permit(alice, bob, allowance, expiry, v, r, s);
@@ -193,7 +193,6 @@ contract PermitTest is Test {
         vm.expectRevert("Bad signatory");
         wrappedToken.permit(alice, bob, allowance, expiry, v, r, s);
 
-
         // bob's allowance from alice is unchanged
         assertEq(wrappedToken.allowance(alice, bob), 0);
 
@@ -214,7 +213,7 @@ contract PermitTest is Test {
         // the expiry block arrives
         vm.warp(expiry);
 
-        // bob calls permit with the signature, but he manipulates the expiry
+        // bob calls permit with the signature after the expiry
         vm.prank(bob);
         vm.expectRevert("Signature expired");
         wrappedToken.permit(alice, bob, allowance, expiry, v, r, s);
@@ -237,7 +236,7 @@ contract PermitTest is Test {
         (, bytes32 r, bytes32 s) = aliceAuthorization(allowance, nonce, expiry);
         uint8 invalidV = 26;
 
-        // bob calls permit with the signature, but he manipulates the expiry
+        // bob calls permit with the signature with invalid `v` value
         vm.prank(bob);
         vm.expectRevert("Invalid value v");
         wrappedToken.permit(alice, bob, allowance, expiry, invalidV, r, s);
@@ -262,7 +261,7 @@ contract PermitTest is Test {
         // 1 greater than the max value of s
         bytes32 invalidS = 0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A1;
 
-        // bob calls permit with the signature, but he manipulates the expiry
+        // bob calls permit with the signature with invalid `s` value
         vm.prank(bob);
         vm.expectRevert("Invalid value s");
         wrappedToken.permit(alice, bob, allowance, expiry, v, r, invalidS);
