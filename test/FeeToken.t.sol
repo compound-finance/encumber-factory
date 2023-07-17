f1b0485e441a32ea954072cc11d14246d8d8b383
Scott Silver <scott@compound.finance>
2023-07-17 14:09:19 -0700

    base FeeToken, FeeTokenTest

---
+++ test/FeeToken.t.sol
@@ -0,0 +1,46 @@
+pragma solidity ^0.8.20;
+
+import "forge-std/StdUtils.sol";
+import { Test } from "forge-std/Test.sol";
+import { EncumberableToken } from "../src/EncumberableToken.sol";
+import { FeeToken } from "../src/test/FeeToken.sol";
+
+contract FeeTokenTest is Test {
+    FeeToken public feeToken;
+    EncumberableToken public wrappedToken;
+
+    address alice = address(10);
+    address bob = address(11);
+    address charlie = address(12);
+
+    function setUp() public {
+        feeToken = new FeeToken("Fee Token", "FEETO");
+        wrappedToken = new EncumberableToken(address(feeToken));
+    }
+
+    function testFeeOnTransfer() public {
+        deal(address(feeToken), alice, 100e18);
+
+        vm.prank(alice);
+        feeToken.transfer(bob, 100e18);
+
+        assertEq(feeToken.balanceOf(alice), 0);
+        assertEq(feeToken.balanceOf(bob), 99e18);
+        assertEq(feeToken.balanceOf(address(feeToken)), 1e18);
+    }
+
+    function testFeeOnTransferFrom() public {
+        deal(address(feeToken), alice, 100e18);
+
+        vm.prank(alice);
+        feeToken.approve(bob, 100e18);
+
+        vm.prank(bob);
+        feeToken.transferFrom(alice, charlie, 100e18);
+
+        assertEq(feeToken.balanceOf(alice), 0);
+        assertEq(feeToken.balanceOf(bob), 0);
+        assertEq(feeToken.balanceOf(charlie), 99e18);
+        assertEq(feeToken.balanceOf(address(feeToken)), 1e18);
+    }
+}
